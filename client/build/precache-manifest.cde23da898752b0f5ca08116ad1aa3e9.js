700d91a475ceae8cba0276b194c5239b3eeb8494
Mohamed Ali <mohamedkosol@gmail.com>
2019-10-15 09:34:53 -0500

    update

---
+++ client/build/precache-manifest.cde23da898752b0f5ca08116ad1aa3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb7e8dcffecfe719e898c1d44489ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b69aad078e210a27b66",
+    "url": "/static/css/main.cef74558.chunk.css"
+  },
+  {
+    "revision": "9eb312171d75ec2cfc01",
+    "url": "/static/js/2.5b6ac498.chunk.js"
+  },
+  {
+    "revision": "5b69aad078e210a27b66",
+    "url": "/static/js/main.97acd2a3.chunk.js"
+  },
+  {
+    "revision": "37facd21dbf1673402a7",
+    "url": "/static/js/runtime~main.ce49a5c8.js"
+  }
+]);
